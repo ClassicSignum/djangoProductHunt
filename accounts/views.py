ff0b0f416310550f364f0857e7ddb643b3380307
nafiz <nafizurrahman001@gmail.com>
2020-08-04 22:58:17 +0600

    login logout & product table migrated

--- accounts/views.py
+++ accounts/views.py
@@ -7,10 +7,12 @@ def signup(request):
     if request.method == 'POST':
         if request.POST['password'] == request.POST['confirmpassword']:
             try:
-                user=User.objects.get(username=request.POST['email'])
-                return render(request,'accounts/signup.html',{'error':'Email already exists'})
+                user=User.objects.get(username=request.POST['username'])
+                return render(request,'accounts/signup.html',{'error':'Username already exists'})
             except User.DoesNotExist:
-                user=User.objects.create_user(request.POST['email'],request.POST['password'])
+                user=User.objects.create_user(username=request.POST['username'],password=request.POST['password'])
+                user.save()
+                print(user)
                 auth.login(request,user)
                 return redirect('homeurl')
         else:
@@ -21,7 +23,22 @@ def signup(request):
      
 
 def login(request):
-     return render(request,'accounts/login.html')
+    if request.method=='POST':
+        user = auth.authenticate(request,username=request.POST['username'],password=request.POST['password'])
+        print(request.POST['username'])
+        print(request.POST['password'])
+        if user is not None:
+            auth.login(request,user)
+            return redirect('homeurl')
+        else:
+            print(user)
+            return render(request,'accounts/login.html',{'error':'username or password is incorrect'})
+    else:
+        return render(request,'accounts/login.html')
+        
 
 def logout(request):
-     return render(request,'accounts/signup.html')
+    if request.method=='POST':
+        auth.logout(request,)
+        return redirect('homeurl')
+     
--- products/models.py
+++ products/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    url = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    icon = models.ImageField(upload_to='images/')
+    votes_total = models.IntegerField(default=1)
+    hunter = models.ForeignKey(User,on_delete=models.CASCADE) # if the user is deleted then the product will also be deleted
+
+    def __str__(self):
+        return self.title
+    
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+
+#now in terminal install pillow....and makemigrations and migrate
